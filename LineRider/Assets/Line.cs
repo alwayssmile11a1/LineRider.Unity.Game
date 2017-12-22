792618d4ca678e06f54cf9ee2f79dfc0bffc9794
2SMILE2 <alwayssmile11a1@gmail.com>
2017-12-22 21:00:39 +0700

    done tutorial

---
+++ LineRider/Assets/Line.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour {
+
+    private List<Vector2> positionList;
+    private EdgeCollider2D edgeCollider2D;
+    private LineRenderer lineRenderer;
+    private bool startUpdate;
+
+    private void Awake() 
+    {
+        edgeCollider2D = GetComponent<EdgeCollider2D>();
+        lineRenderer = GetComponent<LineRenderer>();
+        positionList = new List<Vector2>();
+        startUpdate = false;
+    }
+
+    //private void Update()
+    //{
+    //    if (startUpdate)
+    //    {
+    //        for (int i = 0; i < edgeCollider2D.pointCount; i++)
+    //        {
+
+    //            lineRenderer.SetPosition(i, edgeCollider2D.points[i]);
+    //        }
+          
+    //    }
+    //}
+
+    public void StartUpdate()
+    {
+        startUpdate = true;
+        lineRenderer.useWorldSpace = false;
+
+    }
+
+    public void DrawLineOnMouseMove(Vector2 mousePosition)
+    {
+        if (positionList == null) return;
+        if (positionList.Count == 0) // push to list right away if there is nothing in the list
+        {
+            positionList.Add(mousePosition);
+
+            //set new position to line renderer
+            lineRenderer.positionCount = positionList.Count;
+            lineRenderer.SetPosition(positionList.Count - 1, mousePosition);
+           
+        }
+        else
+        {
+            //check the distance between the previous mouse postion and current mouse position to see if we should put a new position to the list
+            if(Vector2.Distance(mousePosition, positionList[positionList.Count-1]) > 0.1) 
+            {
+                positionList.Add(mousePosition);
+
+                //set new position to line renderer and edge collider
+                lineRenderer.positionCount = positionList.Count;
+                lineRenderer.SetPosition(positionList.Count - 1, mousePosition);
+
+                if (positionList.Count > 1) //set position to edge collider if there are more than 1 position
+                {
+                    edgeCollider2D.points = positionList.ToArray();
+              
+                }
+            }
+        }
+    }
+
+ 
+
+
+}
---
+++ LineRider/Assets/LineCreator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreator : MonoBehaviour {
+
+    public GameObject linePrefab;
+    public GameObject player;
+
+    private GameObject currentLine;
+    private Line lineScript;
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            currentLine = Instantiate(linePrefab);
+            lineScript = currentLine.GetComponent<Line>();
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            lineScript.StartUpdate();
+            currentLine = null;
+        }
+
+
+        if(currentLine!=null)
+        {
+            lineScript.DrawLineOnMouseMove(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
+
+	}
+}
